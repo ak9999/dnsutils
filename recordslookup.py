57aefdd956879cd5a1b23ff115aeda5f370531ef
Abdullah Khan <ajkhan4096@gmail.com>
2019-04-24 13:31:58 -0400

    Add logging.
    Change program name and add exceptions and logging.

--- recordslookup.py
+++ recordslookup.py
@@ -1,28 +1,41 @@
+#!/usr/bin/env python3  
 """
 TODO: module docstring
 """
 
-
+import logging
 import argparse
 from dns.resolver import query
 
 
-def get_records(domain):
-    answers = query(domain, 'TXT')
-    print(f'Query qname: {answers.qname}\t Number of answers: {len(answers)}')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def get_records(domain, record_type):
+    answers = query(domain, record_type)
+    print(f'DNS query for: {answers.qname}')
+    '''
+    Below works for TXT records only. Should search through `answers` for a better result.
+    '''
     for rdata in answers:
-        for txt_string in rdata.strings:
-            print(f'TXT: {txt_string}')
+        for responses in rdata.strings:
+            try:
+                print(f'{record_type}: {responses}')
+            except Exception as e:
+                logger.exception(e)
+                logger.info(f"Could not find record type: {record_type}")
 
 
 def main():
     parser = argparse.ArgumentParser(
-        prog='DNS Parser',
+        prog='recordslookup',
         description='Pull DNS records for a given domain',
     )
-    parser.add_argument('domain', default='google.com')
+    parser.add_argument('domain')
+    parser.add_argument('record_type')
     args = parser.parse_args()
-    get_records(args.domain)
+    get_records(args.domain, args.record_type)
 
 
 if __name__ == '__main__':
